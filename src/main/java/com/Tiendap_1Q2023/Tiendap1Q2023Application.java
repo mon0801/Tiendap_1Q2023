9990c02648d5be5d1e1014cb016ccd4d5831ccbe
Monserrat Najera <Monserrat Najera@LAPTOP-39EVVF51>
2023-01-19 19:41:01 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/Tiendap_1Q2023/Tiendap1Q2023Application.java
@@ -0,0 +1,13 @@
+package com.Tiendap_1Q2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiendap1Q2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tiendap1Q2023Application.class, args);
+	}
+
+}
